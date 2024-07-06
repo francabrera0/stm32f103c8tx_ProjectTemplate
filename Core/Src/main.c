6e17f9a087cd6545579e08e04853fc1dc6ba71bf
francabrera0 <francabrera0@gmail.com>
2024-07-06 20:23:47 -0300

    Change in main

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -214,7 +214,7 @@ static void taskBlink (void *params)
 {
   UNUSED(params);
 
-  int a = add(3, 3);
+  int a = add(2, 2);
   HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, 0);
 
   const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
