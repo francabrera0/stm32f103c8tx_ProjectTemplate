05c2a2ded3ee36fa44a841cbe8aff3f81aca416c
francabrera0 <francabrera0@gmail.com>
2024-07-09 10:53:14 -0300

    Unit tests

---
+++ tests/unitTests/AppUnitTest.c
@@ -0,0 +1,27 @@
+#include <unity.h>
+#include <mylib.h>
+
+void setUp(void) {} /* Is run before every test, put unit init calls here. */
+
+void tearDown(void) {} /* Is run after every test, put unit clean-up calls here. */
+
+void test_sum(void) 
+{
+    TEST_ASSERT_EQUAL(20, add(10, 10));
+    TEST_ASSERT_EQUAL(60, add(50, 10));
+}
+
+void test_sum2(void)
+{
+    TEST_ASSERT_EQUAL(30, add(15, 15));
+}
+
+int main()
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(test_sum);
+    RUN_TEST(test_sum2);
+
+    return UNITY_END();
+}
